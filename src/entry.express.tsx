e34cbc235515d8c5619cb2e2e5f36cc5a090a3cd
Feldrise <admin@feldrise.com>
2026-05-30 16:48:08 +0200

    🔧 Update dependencies in yarn.lock and refactor entry.express.tsx for improved readability

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -8,40 +8,40 @@
  *
  */
 import {
-  createQwikCity,
-  type PlatformNode,
-} from "@builder.io/qwik-city/middleware/node";
-import qwikCityPlan from "@qwik-city-plan";
-import { manifest } from "@qwik-client-manifest";
-import render from "./entry.ssr";
-import express from "express";
-import { fileURLToPath } from "node:url";
-import { join } from "node:path";
+	createQwikCity,
+	type PlatformNode,
+} from '@builder.io/qwik-city/middleware/node';
+import qwikCityPlan from '@qwik-city-plan';
+import {manifest} from '@qwik-client-manifest';
+import render from './entry.ssr';
+import express from 'express';
+import {fileURLToPath} from 'node:url';
+import {join} from 'node:path';
 
 declare global {
-  interface QwikCityPlatform extends PlatformNode {}
+  type QwikCityPlatform = PlatformNode
 }
 
 // Directories where the static assets are located
-const distDir = join(fileURLToPath(import.meta.url), "..", "..", "dist");
-const buildDir = join(distDir, "build");
+const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
+const buildDir = join(distDir, 'build');
 
 // Allow for dynamic port
 const PORT = process.env.PORT ?? 3000;
 
 // Create the Qwik City Node middleware
-const { router, notFound } = createQwikCity({
-  render,
-  qwikCityPlan,
-  manifest,
-  // getOrigin(req) {
-  //   // If deploying under a proxy, you may need to build the origin from the request headers
-  //   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Proto
-  //   const protocol = req.headers["x-forwarded-proto"] ?? "http";
-  //   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
-  //   const host = req.headers["x-forwarded-host"] ?? req.headers.host;
-  //   return `${protocol}://${host}`;
-  // }
+const {router, notFound} = createQwikCity({
+	render,
+	qwikCityPlan,
+	manifest,
+	// getOrigin(req) {
+	//   // If deploying under a proxy, you may need to build the origin from the request headers
+	//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Proto
+	//   const protocol = req.headers["x-forwarded-proto"] ?? "http";
+	//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-Host
+	//   const host = req.headers["x-forwarded-host"] ?? req.headers.host;
+	//   return `${protocol}://${host}`;
+	// }
 });
 
 // Create the express server
@@ -53,8 +53,8 @@ const app = express();
 
 // Static asset handlers
 // https://expressjs.com/en/starter/static-files.html
-app.use(`/build`, express.static(buildDir, { immutable: true, maxAge: "1y" }));
-app.use(express.static(distDir, { redirect: false }));
+app.use(`/build`, express.static(buildDir, {immutable: true, maxAge: '1y'}));
+app.use(express.static(distDir, {redirect: false}));
 
 // Use Qwik City's page and endpoint request handler
 app.use(router);
@@ -64,6 +64,6 @@ app.use(notFound);
 
 // Start the express server
 app.listen(PORT, () => {
-  /* eslint-disable */
+	/* eslint-disable */
   console.log(`Server started: http://localhost:${PORT}/`);
 });
